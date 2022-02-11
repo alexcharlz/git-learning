a913ef7f0ac3c1720ef2d53ff1a657a89e23145e
Gabriel_Anthony <idenyigabriel@gmail.com>
2022-02-11 12:24:04 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,7 @@
+from getpass import getpass
+
+username = str(input("Enter your username: "))
+password = getpass("Enter your Password: ")
+if username == "gabriel" and password == "password":
+    print("authenticated")
+print("not authenticated")
